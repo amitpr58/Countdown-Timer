0bf0e009ea0fd297438f2d3306e4b759dc5a436e
Amit Prajapati <111772936+amitpr58@users.noreply.github.com>
2024-02-24 12:28:19 +0530

    Add files via upload

---
+++ digitalClock.js
@@ -0,0 +1,33 @@
+
+
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+  
+    if (h == 0) {
+        h = 12;
+    }
+  
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+  
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+  
+    document.getElementById("hours").value = h;
+    document.getElementById("minutes").value = m;
+    document.getElementById("seconds").value = s;
+    document.getElementById("session").value = session;
+  
+    setTimeout(showTime, 1000);
+  }
+  
+  showTime();
+  
